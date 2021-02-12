e9a4505c11b78d049bd729afb491c5416ecadd72
cetherdev <robert.hct@gmail.com>
2021-02-11 21:07:37 -0600

    Optimizando el Build de la aplicación (mimificando)

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+
+require("dotenv").config();
+
+const isDev = process.env.ENV === "development";
+const entry = ["./src/frontend/index.js"];
+
+if (isDev) {
+  entry.push(
+    "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true"
+  );
+}
 
 module.exports = {
-  entry: [
-    "./src/frontend/index.js",
-    "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true",
-  ],
-  mode: "development",
+  entry,
+  mode: process.env.ENV,
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "src/server/public"),
     filename: "assets/app.js",
     publicPath: "/",
   },
@@ -25,14 +33,6 @@ module.exports = {
         use: {
           loader: "babel-loader",
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
       },
       {
         test: /\.(s*)css$/,
@@ -61,7 +61,13 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    isDev ? new webpack.HotModuleReplacementPlugin() : () => {},
+    isDev
+      ? () => {}
+      : new CompressionWebpackPlugin({
+          test: /\.js$|\.css$/,
+          filename: "[path].gz",
+        }),
     new MiniCssExtractPlugin({
       filename: "assets/app.css",
     }),
